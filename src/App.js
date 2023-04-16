43b6b01089e461f3d459e9c332f89d3040b251ac
Ali Ahmad <mian41935@gmail.com>
2023-04-17 01:08:39 +0500

    React Server Side Rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
